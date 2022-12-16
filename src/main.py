c1685e391cc295e650f589e75efd3ed13219f0e5
NeoZhangJianyu <jianyu.zhang@intel.com>
2022-12-16 16:59:13 +0800

    for test

---
+++ src/main.py
@@ -0,0 +1,15 @@
+
+
+def main():
+    """this is main() function
+
+    no parameter
+
+    """
+    return 1
+
+def test():
+    """this is test
+
+    no parameter
+    """
---
+++ src/model/fun.py
@@ -0,0 +1,15 @@
+
+
+def fun1():
+    """this is fun1() function
+
+    no parameter
+
+    """
+    return 1
+
+def test1():
+    """this is test1
+
+    no parameter
+    """
