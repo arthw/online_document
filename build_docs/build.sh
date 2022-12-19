63b07257e1731941abc82012497e06d70a04bfbe
ZhangJianyu <zhang.jianyu@outlook.com>
2022-12-19 22:10:58 +0800

    update

--- build_docs/build.sh
+++ build_docs/build.sh
@@ -24,12 +24,13 @@ else
   exit 1
 fi
 
-git config --global user.email "zhang.jianyu@outlook.com"
-git config --global user.name "ZhangJianyu"
-
 
 git checkout -b gh-pages
 git branch --set-upstream-to=origin/gh-pages gh-pages
+
+git config --global user.email "zhang.jianyu@outlook.com"
+git config --global user.name "ZhangJianyu"
+
 git pull
 git fetch origin
 git reset --hard origin/gh-pages
